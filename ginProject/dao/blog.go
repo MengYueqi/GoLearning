810c5c7f7fdb499a2d23fa8b84874e497259dc94
MengYueqi <mengyq2002@gmail.com>
2024-08-28 21:09:33 +0800

    add join select

--- ginProject/dao/blog.go
+++ ginProject/dao/blog.go
@@ -11,15 +11,19 @@ type Blogs struct {
 	Id        int `gorm:"primary_key;AUTO_INCREMENT"`
 	CreatedAt time.Time
 	AuthorId  int
+	Username  string
 	Content   string
 }
 
 func GetAllBlogsById(AuthorId int) ([]*Blogs, error) {
 	var blogs []*Blogs
 	fmt.Println(AuthorId)
-	result := db.Where("author_id = ?", AuthorId).Find(&blogs)
+	//result := db.Where("author_id = ?", AuthorId).Find(&blogs)
+	result := db.Table("blogs").Select("blogs.id, blogs.created_at, blogs.author_id, blogs.content, users.username").
+		Joins("JOIN users ON users.user_id = blogs.author_id").Where("blogs.author_id = ?", AuthorId).Find(&blogs)
 	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
 		return nil, nil
 	}
+	fmt.Println(blogs[0].Username)
 	return blogs, result.Error
 }
