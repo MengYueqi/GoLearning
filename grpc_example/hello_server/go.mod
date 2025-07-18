178996d2b8fbdce9a0cbab5afecebf9d85ec1c27
MengYueqi <mengyq2002@gmail.com>
2025-07-18 17:31:52 +0800

    add some code about consul

--- grpc_example/hello_server/go.mod
+++ grpc_example/hello_server/go.mod
@@ -1,22 +1,40 @@
 module hello_server
 
-go 1.23.0
+go 1.23.8
+
+toolchain go1.23.11
 
 require (
 	github.com/golang/protobuf v1.5.4
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.27.1
+	github.com/hashicorp/consul/api v1.32.1
 	github.com/mennanov/fieldmask-utils v1.1.2
 	google.golang.org/grpc v1.73.0
 )
 
 require (
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.27.1 // indirect
+	github.com/armon/go-metrics v0.4.1 // indirect
+	github.com/fatih/color v1.16.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-hclog v1.5.0 // indirect
+	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/hashicorp/serf v0.10.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	golang.org/x/exp v0.0.0-20250305212735-054e65f0b394 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20250603155806-513f23925822 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250603155806-513f23925822 // indirect
 )
 
 require (
 	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/net v0.38.0 // indirect
+	golang.org/x/net v0.38.0
 	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.26.0 // indirect
 	google.golang.org/genproto v0.0.0-20250324211829-b45e905df463 // indirect
--- grpc_example/hello_server/main.go
+++ grpc_example/hello_server/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/golang/protobuf/protoc-gen-go/generator"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/hashicorp/consul/api"
 	fieldmask_utils "github.com/mennanov/fieldmask-utils"
 	pb "github.com/testProject/pb"
 	_ "github.com/testProject/pb/author"
@@ -13,6 +14,8 @@ import (
 	"golang.org/x/net/http2/h2c"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/health"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 	"io"
 	"log"
 	"net"
@@ -25,6 +28,22 @@ import (
 
 type server struct {
 	pb.UnimplementedGreeterServer
+}
+
+// consul 定义一个consul结构体，其内部有一个`*api.Client`字段。
+type consul struct {
+	client *api.Client
+}
+
+// NewConsul 连接至consul服务返回一个consul对象
+func NewConsul(addr string) (*consul, error) {
+	cfg := api.DefaultConfig()
+	cfg.Address = addr
+	c, err := api.NewClient(cfg)
+	if err != nil {
+		return nil, err
+	}
+	return &consul{c}, nil
 }
 
 // BookServiceImpl 是你的服务实现
@@ -102,6 +121,20 @@ func main() {
 	bookpb.RegisterBookServiceServer(s, &BookServiceImpl{})
 	pb.RegisterGreeterServer(s, &server{})
 
+	// 注册健康检查服务
+	healthcheck := health.NewServer()
+	healthpb.RegisterHealthServer(s, healthcheck)
+
+	conImpl, err := NewConsul("127.0.0.1:8500")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	localIP, err := GetOutboundIP()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	conImpl.RegisterService("BookAndHello", localIP.String(), 8091)
+
 	// gRPC-Gateway mux
 	gwmux := runtime.NewServeMux()
 	dops := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
@@ -135,4 +168,40 @@ func grpcHandlerFunc(grpcServer *grpc.Server, otherHandler http.Handler) http.Ha
 			otherHandler.ServeHTTP(w, r)
 		}
 	}), &http2.Server{})
+}
+
+// GetOutboundIP 获取本机的出口IP
+func GetOutboundIP() (net.IP, error) {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	return localAddr.IP, nil
+}
+
+// RegisterService 将gRPC服务注册到consul
+func (c *consul) RegisterService(serviceName string, ip string, port int) error {
+	localIP, err := GetOutboundIP()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	check := &api.AgentServiceCheck{
+		GRPC:     fmt.Sprintf("%s:%d", localIP.String(), port), // 这里一定是外部可以访问的地址
+		Timeout:  "10s",                                        // 超时时间
+		Interval: "10s",                                        // 运行检查的频率
+		// 指定时间后自动注销不健康的服务节点
+		// 最小超时时间为1分钟，收获不健康服务的进程每30秒运行一次，因此触发注销的时间可能略长于配置的超时时间。
+		DeregisterCriticalServiceAfter: "1m",
+	}
+	srv := &api.AgentServiceRegistration{
+		ID:      fmt.Sprintf("%s-%s-%d", serviceName, ip, port), // 服务唯一ID
+		Name:    serviceName,                                    // 服务名称
+		Tags:    []string{"Meng", "hello", "books"},             // 为服务打标签
+		Address: ip,
+		Port:    port,
+		Check:   check,
+	}
+	return c.client.Agent().ServiceRegister(srv)
 }
